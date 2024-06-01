01bc1a71d2c436e192b648ec0468413793bb50f0
Damilola Olowookere <damms005@gmail.com>
2024-06-01 17:11:50 +0100

    Install filament

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Todo;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/todos/store', function () {
+    Todo::create([
+        'user_id' => 1,
+        'title' => request('data'),
+    ]);
 });
